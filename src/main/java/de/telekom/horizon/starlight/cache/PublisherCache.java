b3ce5d804007fc02e1c4c85f16c29a68c3517402
Mike Herwig <Mike.Herwig@telekom.de>
2024-04-29 18:55:38 +0200

    fix: use correct environment when cache query is executed

--- src/main/java/de/telekom/horizon/starlight/cache/PublisherCache.java
+++ src/main/java/de/telekom/horizon/starlight/cache/PublisherCache.java
@@ -8,30 +8,39 @@ import de.telekom.eni.pandora.horizon.cache.service.JsonCacheService;
 import de.telekom.eni.pandora.horizon.cache.util.Query;
 import de.telekom.eni.pandora.horizon.exception.JsonCacheException;
 import de.telekom.eni.pandora.horizon.kubernetes.resource.SubscriptionResource;
+import de.telekom.horizon.starlight.config.StarlightConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Slf4j
 @Component
 public class PublisherCache {
 
+    private final StarlightConfig starlightConfig;
+
     private final JsonCacheService<SubscriptionResource> subscriptionCache;
 
-    public PublisherCache(JsonCacheService<SubscriptionResource> subscriptionCache) {
+    public PublisherCache(StarlightConfig starlightConfig, JsonCacheService<SubscriptionResource> subscriptionCache) {
+        this.starlightConfig = starlightConfig;
         this.subscriptionCache = subscriptionCache;
     }
 
     public Set<String> findPublisherIds(String environment, String eventType) {
+        var env = environment;
+        if (Objects.equals(starlightConfig.getDefaultEnvironment(), environment)) {
+            env = "default";
+        }
+
         var builder = Query.builder(SubscriptionResource.class)
-                .addMatcher("spec.environment", environment)
+                .addMatcher("spec.environment", env)
                 .addMatcher("spec.subscription.type", eventType);
 
         List<SubscriptionResource> list;
-
         try {
             list = subscriptionCache.getQuery(builder.build());
         } catch (JsonCacheException e) {
@@ -44,7 +53,11 @@ public class PublisherCache {
 
         list.forEach(a -> {
             publisherIds.add(a.getSpec().getSubscription().getPublisherId());
-            publisherIds.addAll(a.getSpec().getSubscription().getAdditionalPublisherIds());
+
+            var additionalPublisherIds = a.getSpec().getSubscription().getAdditionalPublisherIds();
+            if (additionalPublisherIds != null) {
+                publisherIds.addAll(additionalPublisherIds);
+            }
         });
 
         return publisherIds;
