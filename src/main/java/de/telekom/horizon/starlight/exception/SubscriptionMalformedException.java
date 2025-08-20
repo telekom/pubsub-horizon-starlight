88b5a089277ccda3522a21b9fbbac2d6064e17a2
Matthias Schneider <matthias.schneider04@telekom.de>
2025-08-20 15:30:56 +0200

    chore(subscription-cache): add license information to new files

--- src/main/java/de/telekom/horizon/starlight/exception/SubscriptionMalformedException.java
+++ src/main/java/de/telekom/horizon/starlight/exception/SubscriptionMalformedException.java
@@ -1,3 +1,7 @@
+// Copyright 2024-2025 Deutsche Telekom IT GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package de.telekom.horizon.starlight.exception;
 
 public class SubscriptionMalformedException extends RuntimeException {
